0227e92e8f94e82130b5cdbfd94f0ed92d0d547b
John Gronberg <gronberg@gmail.com>
2012-06-17 22:32:55 -0700

    chapter 5 exercises (better specs

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,12 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# removed the below by adding a test for the application helper... and then 
+# just including application helpers
+# def full_title(page_title)
+#   base_title = "Ruby on Rails Tutorial Sample App"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end
