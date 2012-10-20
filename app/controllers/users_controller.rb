9715464ddeb4952fb5abdab91840a1db256e946c
John Gronberg <jgronberg@John-Gronbergs-Macbook-Air.local>
2012-10-19 17:19:05 -0700

    beginning microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -66,13 +67,14 @@ class UsersController < ApplicationController
   end
 
   private
-  
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+
+    # removed in section 10.2.2  
+    # def signed_in_user
+    #   unless signed_in?
+    #     store_location
+    #     redirect_to signin_url, notice: "Please sign in."
+    #   end
+    # end
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
